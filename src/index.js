e79daac1d8efc4abd3f60f99e9b1e22a97d8bc28
os-born <ol.serduk@gmail.com>
2021-08-09 20:30:52 +0300

    проверки

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import './js/themeSwitcher';
 import './sass/main.scss';
 
 const defaultTheme = refs.body.classList.add('light-theme');
-// if (){};
 
-defaultTheme
+
+if (savedData === 'light-theme'){
+    defaultTheme;
+};
+
+if (savedData === 'dark-theme'){
+    refs.themeSwitcher.checked
+    refs.body.classList.replace('light-theme', 'dark-theme')
+};
+
 
